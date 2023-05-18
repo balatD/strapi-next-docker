e76948b3648a2b386c140e48916bc42678910eaf
Dragan Balatinac <balatinac@cp-compartner.de>
2023-05-18 11:59:36 +0200

    Refactoring

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ articles, error }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +13,51 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Wooo!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/yeah</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {articles.data.map(articles => (
+            <div id={articles.id}>
+              <h3>Title: {articles.attributes.title}</h3>
+              Description: {articles.attributes.body}
+            </div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
       </footer>
     </div>
   )
 }
+
+Home.getInitialProps = async ctx => {
+  try {
+    // Parses the JSON returned by a network request
+    const parseJSON = resp => (resp.json ? resp.json() : resp);
+    // Checks if a network request came back fine, and throws an error if not
+    const checkStatus = resp => {
+      if (resp.status >= 200 && resp.status < 300) {
+        return resp;
+      }
+
+      return parseJSON(resp).then(resp => {
+        throw resp;
+      });
+    };
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const articles = await fetch('http://backend:1337/api/articles?populate=*', {
+      method: 'GET',
+      headers,
+    })
+        .then(checkStatus)
+        .then(parseJSON);
+
+    return { articles };
+  } catch (error) {
+    return { error };
+  }
+};
+
 
 export default Home
