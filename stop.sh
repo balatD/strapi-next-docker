6233a95dea74865dc835df273c7381beab120f9d
Dragan Balatinac <balatinac@cp-compartner.de>
2023-05-19 14:30:50 +0200

    Add stop command

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
+
+docker-compose down -d
+
+echo
+echo -e "${GREEN}Stopping $PROJECT_URL ...${NC}"
+echo
