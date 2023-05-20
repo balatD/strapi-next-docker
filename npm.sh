43237e9ea8af0b9320822f9b6595ba773890db9c
Dragan Balatinac <balatinac@cp-compartner.de>
2023-05-21 00:34:41 +0200

    Add third parameter option to npm.sh to allow passing arguments to npm command inside chosen service

--- npm.sh
+++ npm.sh
@@ -5,22 +5,22 @@ NC='\033[0m'
 
 [ ! -f .env ] || export $(grep -v '^#' .env | xargs)
 
-# Check if the required arguments are provided
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <service> <npm_command>"
+if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+  echo "Usage: $0 <container_name> <npm_command> [npm_arguments]"
   exit 1
 fi
 
 SERVICE=$1
 NPM_COMMAND=$2
+NPM_COMMAND_ARGS=$3
 
 echo
-echo -e "${GREEN}Executing 'npm ${NPM_COMMAND}' within the ${SERVICE} container of ${PROJECT_NAME}.${NC}"
+echo -e "${GREEN}Executing 'npm ${NPM_COMMAND} ${NPM_COMMAND_ARGS}' within the ${SERVICE} container of ${PROJECT_NAME}.${NC}"
 echo
 
 CONTAINER_ID=$(docker-compose ps -q "$SERVICE")
 
-docker exec -it "$CONTAINER_ID" npm "$NPM_COMMAND"
+docker exec -it "$CONTAINER_ID" npm "$NPM_COMMAND" "$NPM_COMMAND_ARGS"
 
 echo
 echo -e "${GREEN}npm ${NPM_COMMAND} completed.${NC}"
